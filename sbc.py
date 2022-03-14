4dd3127a5cb9cb040bafe7dde89837b0e1648755
Athanasios Anastasiou <athanastasiou@gmail.com>
2022-03-14 23:35:20 +0100

    With exceptions to collect error messages

--- sbc.py
+++ sbc.py
@@ -7,8 +7,22 @@ Springboard compiler
 
 """
 import os
+import sys
 import pyparsing
 import click
+
+
+class SpringboardError(Exception):
+    pass
+
+
+class SpringboardSymbolRedefined(SpringboardError):
+    pass
+
+
+class SpringboardCircularDependency(SpringboardError):
+    pass
+
 
 class SpringboardProgram:
     def __init__(self):
@@ -28,26 +42,36 @@ class SpringboardProgram:
     def code(self):
         return self._code_info[0]["code"]
 
-    def from_string(self, a_program):
+    def from_string(self, a_program, previous_imports=[]):
         self._code_info = self._parser.parseString(a_program)
         # If there are imports, prepopulate the symbol definition table
         cwd = os.getcwd()
+        p_imports=previous_imports + []
         for an_import in self.imports:
+            # Break on circular references
+            if an_import not in p_imports:
+                p_imports.append(an_import)
+            else:
+                click.echo(f"Please resolve the circular dependency involving {an_import} and {','.join(previous_imports)} and try again.")
+                sys.exit(1)
+            # Change the current working directoy to enable relative imports
             import_path, import_file = os.path.split(an_import)
             if len(import_path) > 0:
                 os.chdir(import_path)
-            u = SpringboardProgram().from_file(import_file)
+            u = SpringboardProgram().from_file(import_file, p_imports)
             os.chdir(cwd)
             self._symbol_defs.update(u.symbol_defs)
+
         # Append the locally defined symbols
         for a_symbol_def in self._code_info[0]["symbol_defs"]:
             self._symbol_defs[a_symbol_def["symbol"]] = a_symbol_def["code"]
         return self
 
-    def from_file(self, a_file):
+    def from_file(self, a_file, previous_imports=[]):
+        p_imports = previous_imports + []
         with open(a_file, "rt") as fd:
             data = fd.read()
-        return self.from_string(data)
+        return self.from_string(data, p_imports)
 
     def compile(self, a_program=None):
         bf_code = "+-.,<>[]"
